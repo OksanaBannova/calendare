95e857ee951deb145b0d903214f5855427e3ef21
Oksana Bannova <oksanchik2170@yandex.ru>
2026-04-03 17:33:21 +0300

    дфиуд

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/wall-calendar/", // имя репозитория
+  base: "/calendare/", // имя репозитория
   plugins: [react()]
 });
